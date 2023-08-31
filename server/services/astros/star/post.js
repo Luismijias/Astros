642695c79384564a0bd561451daa67a09ac9432b
Luismijias <luismijias@gmail.com>
2023-08-30 23:12:06 -0300

    commited whith script by pc ubunto_main.

--- server/services/astros/star/post.js
+++ server/services/astros/star/post.js
@@ -1,9 +1,10 @@
-const dbStar = _db.queryFirst('SELECT * FROM star WHERE lower(name) = lower(?)' ,
- _req.getString('name')
+const dbStar = _db.queryFirst(
+  'SELECT * FROM star WHERE lower(name) = lower(?)' ,
+ _req.getString('nome')
  )
 
 if (dbStar) {
-    _header.status(409)
+    _header.status(400)
     _out.json(
         _val.map()
             .set('erro', true)
@@ -22,7 +23,7 @@ _db.insert(
         .set('name', nome)
         .set('rotation', rotacao)
         .set('radius', raio)
-);
+)
 
 _out.json(
     _val.map()
